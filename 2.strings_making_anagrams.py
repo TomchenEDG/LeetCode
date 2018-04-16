a5bab1b589d19c92e24d845b0c517519d55f388b
cwy <410903874@qq.dcom>
2018-04-17 07:17:00 +0800

    Implementation code.

--- 2.strings_making_anagrams.py
+++ 2.strings_making_anagrams.py
@@ -1,13 +1,38 @@
 def buildMap(s):
+    """
+    The number of occurrences of a string in a dictionary.
+    """
     the_map = {}
     for char in s:
         if char not in the_map:
             the_map[char] = 1
         else:
-            the_map[char] +=1
+            the_map[char] += 1
 
     return the_map
 
+def anagram(s1, s2):
+    """
+    If two strings appear different characters, the number of occurrences of that character is recorded;
+    If the same character appears, compare the number of times the character appears;
+    """
+    map1 = buildMap(s1)
+    map2 = buildMap(s2)
+
+    diff_cnt = 0
+    for key in map2.keys():
+        if key not in map1:
+            diff_cnt += map2[key]
+        else:
+            diff_cnt += max(0, map2[key]-map1[key])
+
+    for key in map1.keys():
+        if key not in map2:
+            diff_cnt += map1[key]
+        else:
+            diff_cnt += max(0, map1[key]-map2[key])
+
+    return diff_cnt
 
 if __name__ == '__main__':
     s1 = raw_input()
