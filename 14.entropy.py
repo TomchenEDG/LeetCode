613b2df5c318873ff4607074ddaa6c5ad0c39c93
Tom Chen <samchenedg@gmail.com>
2019-07-22 12:23:47 +0800

    Add 单个数 和 特征的信息熵计算

---
+++ 14.entropy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+
+
+# 第一种：计算单个数的信息熵
+import numpy as np
+import matplotlib.pyplot as plt
+
+def entropy(p):
+    return -p*np.log(p) -(1-p)*np.log(1-p)
+
+x = np.linspace(0.01, 0.99, 200)
+
+plt.plot(x, entropy(x))
+plt.show()
+
+
+
+# 第二种：计算特征的信息熵
+# def entropy(y):
+#     counter = Counter(y)
+#     res = 0.0
+#     for num in counter.values():
+#         p = num / len(y)
+#         res += -p * log(p)
+#     return res
